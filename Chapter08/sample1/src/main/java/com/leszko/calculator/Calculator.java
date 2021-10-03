979643cef1cafd3109fe5ee3fb25c1017e28d5d4
naren <narenvishu@gmail.com>
2021-10-03 19:02:51 -0400

    rollback config to verfify checkstyle error

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,9 +3,6 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-/**
- * Calculator class.
- */
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
